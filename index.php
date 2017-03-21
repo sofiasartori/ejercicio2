50ea85bb189073b3b48bda105bbceae0c7eab833
alumno <alumno>
2017-03-20 21:14:52 -0300

    Primer manera de array OK

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
     echo "Hola Mundo";
+
+    /*Primer manera: constructor de array
+    Segunda manera: lo mismo pero con corchetes (en for o lineas separadas)
+    Tercer manera: .push() para cada uno de los elementos, con for o linea a linea
+    var_dump(array) para mostrarlo
+    */
+    
+    //Primer manera
+
+    $miArray = array(rand(), rand(), rand(), rand(), rand());
+    var_dump($miArray);
+
+    //Segunda manera
+
 ?>
