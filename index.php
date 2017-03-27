a3be14067570f8e68cdbca184837ffde9c44d685
alumno <alumno>
2017-03-27 19:04:41 -0300

    Ejercicios 2 y 3 de la guia

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
-    echo "Hola Mundo";
+    echo "Ejercicio 1<br>";
+
+    echo "Ejercicio 2<br>";
+
+    echo date("y-M-d");
+    $mes = date('m');
+    echo '<br>';
+
+    if($mes == 'Jan' || $mes == 'Feb' || $mes =='Dec')
+        echo 'estamos en verano<br>';
+    else if($mes == 'Mar' || $mes == 'Apr' || $mes =='May')
+        echo 'estamos en otonio<br>';
+    else if($mes == 'Jun' || $mes == 'Jul' || $mes =='Ago')
+        echo 'estamos en invierno<br>';
+    else
+        echo 'estamos en primavera<br>';
+    
+    echo "Ejercicio 3<br>";
+
+    $numero1 = rand();
+    $numero2 = rand();
+    $numero3 = rand();
+
+    echo 'Numero 1: '.$numero1;
+    echo '<br>Numero 2: '.$numero2;
+    echo '<br>Numero 3: '.$numero3;
+
+    if ($numero1<$numero2 && $numero1>$numero3 || $numero1>$numero2 && $numero1<$numero3)   
+        echo '<br>'.$numero1;
+    else if($numero2<$numero1 && $numero2>$numero3 || $numero2>$numero1 && $numero2<$numero3)
+        echo '<br>'.$numero2;
+    else
+        echo '<br>'.$numero3;
+
+    echo '<br>Ejercicio 4';
+
+    
+
+    echo "<br>Ejercicio 6";
 
     /*Primer manera: constructor de array
     Segunda manera: lo mismo pero con corchetes (en for o lineas separadas)
